97aef54e03e004ef09ebe845163737e9c90e8151
zeenat <zeenu.23@gmail.com>
2024-02-27 16:19:29 +0530

    refactored code to use Unsplash API for image data and display.

--- src/js/index.js
+++ src/js/index.js
@@ -1,155 +1,114 @@
-// The following code starts Mock Service Worker tool which allows you to simulate a backend (an API) for building your apps that talk to a remote service. You can visit https://mswjs.io for details on this utility and check src/api/routes.js for a sample API route that you can edit/create as needed to simulate a real world API. This simulated backend will not be part of the completed application (built edition) and you must use a real world backend built using Node.js + Express or Java + Spring Boot to provide such a service.
+import { createApi } from "unsplash-js";
 
-// If you do not require a simulated backend, you can remove the code shown below.
+const main = document.querySelector("#main");
+const unsplash = createApi({
+  accessKey: "nzFQzuv0f9jWK6bWKrRoWdwwy-YQM2bG_MNubhH_l_U",
+});
 
-// const apiStatus = document.querySelector("#api-status");
+const createGalleryFromFavorites = (favourites) => {
+  // favourites is an array containing the favorite photos
+  const getUrls = favourites.map((i) => {
+    return `<div class="gallery-item" data-title="${i.title}"><img src="${i.urls.small}"/><p class="heart">&#10084;</p></div>`;
+  });
+  main.innerHTML = getUrls.join("");
 
-// if (import.meta.env.DEV) {
-//   import("../api/browser")
-//     .then(({ worker }) => worker.start())
-//     .then(() => fetch("/"))
-//     .then((res) => res.json())
-//     .then((res) => (apiStatus.innerText = res.message));
-// }
-
-const API_URL = "https://api.unsplash.com";
-const UNSPLASH_ACCESS_KEY = "9eUvuW14SeKzJLwV5qB7LLIOg50rsc5cZaeIcX7ZGf8";
-
-// Function to render data in cards
-
-const renderData = (images) => {
-  const grid = document.querySelector(".grid");
-
-  images.forEach((image) => {
-    const container = document.createElement("div");
-    const imageElement = document.createElement("img");
-
-    const card = document.createElement("div");
-    card.classList.add("card");
-
-    const title = document.createElement("h2");
-    title.textContent = image.title;
-
-    const body = document.createElement("p");
-    body.textContent = image.body;
-
-    imageElement.src = image?.urls?.regular;
-    // imageElement.alt = image.alt_description || "";
-    // imageElement.description = image.description || "";
-
-    container.append(imageElement);
-    grid.append(container);
-
-    card.appendChild(title);
-    card.appendChild(body);
-    container.appendChild(card);
+  // Add event listeners to the hearts in the gallery items for favorites
+  const galleryItems = document.querySelectorAll(".gallery-item");
+  galleryItems.forEach((item, index) => {
+    const heart = item.querySelector(".heart");
+    heart.addEventListener("click", (e) => {
+      e.preventDefault();
+      addToFavourites(favourites, index); // Pass all the image details to the favorites section
+    });
   });
 };
 
-//fetch data
-const fetchData = async () => {
-  const res = await fetch(
-    `${API_URL}/photos/?client_id=${UNSPLASH_ACCESS_KEY}&per_page=30`
-  );
-  if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`);
-  const data = await res.json();
-  console.log(data);
-
-  const images = data.map((image) => image?.urls?.regular);
-  console.log(images);
-  renderData(images);
+const addToFavourites = (photo) => {
+  const favourites = JSON.parse(localStorage.getItem("favourites")) || [];
+  favourites.push(photo);
+  localStorage.setItem("favourites", JSON.stringify(favourites));
+  // console.log(favourites);
+  // console.log("added to favourites");
+  // console.log(photo);
 };
-fetchData();
 
-document.addEventListener("DOMContentLoaded", function () {
-  const homeLink = document.getElementById("home");
-  const modernArtLink = document.getElementById("modern-art");
-  const classicsLink = document.getElementById("classics");
-  const sculpturesLink = document.getElementById("sculptures");
-  const cubismLink = document.getElementById("cubism");
-  const abstractArtLink = document.getElementById("abstract-art");
-  const favouritesLink = document.getElementById("favourites");
+const createGallery = (filterValue) => {
+  console.log(filterValue);
+  if (filterValue == "Favourites") {
+    // Create gallery using the favourites array
+    createGalleryFromFavorites(JSON.parse(localStorage.getItem("favourites")));
+  } else {
+    unsplash.search
+      .getPhotos({
+        query: filterValue,
+        page: 2,
+        perPage: 16,
+        orientation: "portrait",
+      })
+      .then((result) => {
+        if (result.type === "success") {
+          const photos = result.response.results;
+          console.log(photos);
+          const getUrls = photos.map((i) => {
+            return `<div class="gallery-item" data-title="${i.description}" data-description="${i.alt_description}" data-author="${i.user.username}"><img src="${i.urls.small}"/></><p class="heart">&#10084;</p></div>`;
+          });
+          main.innerHTML = getUrls.join("");
 
-  homeLink.addEventListener("click", showHome);
-  modernArtLink.addEventListener("click", showModernArt);
-  classicsLink.addEventListener("click", showClassics);
-  sculpturesLink.addEventListener("click", showSculptures);
-  cubismLink.addEventListener("click", showCubism);
-  abstractArtLink.addEventListener("click", showAbstractArt);
-  favouritesLink.addEventListener("click", showFavourites);
+          // Add event listeners to the heart
+          const hearts = document.querySelectorAll(".heart");
+          hearts.forEach((heart, index) => {
+            heart.addEventListener("click", (e) => {
+              e.preventDefault();
+              addToFavourites(photos[index]);
+            });
+          });
 
-  function showHome(e) {
+          // Add event listeners to the gallery items
+          const galleryItems = document.querySelectorAll(".gallery-item");
+          galleryItems.forEach((item) => {
+            item.addEventListener("click", (e) => {
+              e.preventDefault();
+              let title = item.getAttribute("data-title");
+              if (title == "null") {
+                title = "No title Available";
+              }
+              const description = item.getAttribute("data-description");
+              const author = item.getAttribute("data-author");
+
+              console.log("clicked");
+              const imageDetails = document.querySelector(".imageDetails");
+              imageDetails.style.display = "flex";
+              imageDetails.innerHTML = `
+            <img src="${e.target.src}" alt="" />
+            <p class="imageHeart">&#10084;</p>
+            <p class="title">${title}</p>
+            <p class="description">${description}</p>
+            <p class="category">${filterValue}</p>
+            <p class="author">${author}</p>
+            <p class="close">X</p>
+          `;
+
+              const close = document.querySelector(".close");
+              close.addEventListener("click", (e) => {
+                imageDetails.style.display = "none";
+
+                console.log("clicked");
+              });
+            });
+          });
+        }
+      });
+  }
+};
+
+createGallery("Featured");
+
+let filter = document.getElementsByClassName("filter");
+Array.from(filter).forEach((element) => {
+  element.addEventListener("click", (e) => {
     e.preventDefault();
-    grid.innerHTML =
-      "<h2>Welcome to Artify!</h2><p>Explore our collection of art.</p>";
-  }
-
-  function showModernArt(e) {
-    e.preventDefault();
-    fetchArtworks("modern-art").then((artworks) => displayArtworks(artworks));
-  }
-
-  function showClassics(e) {
-    e.preventDefault();
-    fetchArtworks("classics").then((artworks) => displayArtworks(artworks));
-  }
-
-  function showSculptures(e) {
-    e.preventDefault();
-    fetchArtworks("sculptures").then((artworks) => displayArtworks(artworks));
-  }
-
-  function showCubism(e) {
-    e.preventDefault();
-    fetchArtworks("cubism").then((artworks) => displayArtworks(artworks));
-  }
-
-  function showAbstractArt(e) {
-    e.preventDefault();
-    // Use the search functionality with an empty query parameter to get all works in the "abstract" from the above unsplash API
-    fetchArtworks("abstract-art").then((images) => {
-      console.log(images);
-      let imageIndex = Math.floor(Math.random() * images.length);
-      window.location.href = `/image?id=${images[imageIndex]._id}`;
-
-      let filteredArtworks = images.filter(
-        (image) => image.is_abstract === true
-      );
-      displayArtworks(filteredArtworks);
-    });
-  }
-
-  async function showFavourites(images) {
-    let userFavourites = JSON.parse(localStorage.getItem("favourites")) || [];
-    let favourites = images.filter((image) => image.liked_by_user);
-    userFavourites = favourites.map((image) => image.id);
-
-    localStorage.setItem("userFavourites", JSON.stringify(userFavourites));
-
-    console.log("User favourites:", favourites);
-  }
-
-  function fetchArtworks(category) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        const artworks = [
-          { title: "Painting 1", artist: "Artist 1", imageUrl: "image1.jpg" },
-          { title: "Painting 2", artist: "Artist 2", imageUrl: "image2.jpg" },
-          // Add more artworks here
-        ];
-        resolve(artworks);
-      }, 1000);
-    });
-  }
-
-  function displayArtworks(artworks) {
-    let html = "";
-    artworks.forEach((artwork) => {
-      html += `<div class="artwork">
-                        <img src="${artwork.imageUrl}" alt="${artwork.title}">
-                        <p>${artwork.title} by ${artwork.artist}</p>
-                    </div>`;
-    });
-    grid.innerHTML = html;
-  }
+    console.log(e.target.innerText);
+    let filterValue = e.target.innerText;
+    createGallery(filterValue);
+  });
 });
